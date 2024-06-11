e847efa24aeeeb92c11ab8ea2cf137705dfe8a85
JulietMumbi <juliet.ndambuki@strathmore.edu>
2024-06-11 18:02:02 +0300

    changed my navigations to php

--- templates/nav.php
+++ templates/nav.php
@@ -1,10 +1,10 @@
 <div class="topnav">
-        <a href="index.html">Home</a>
-        <a href="about.html">About us</a>
-        <a href="contact.html">Contact us</a>
-        <a href="Terms and Conditions.html">Terms & Conditions</a>
-        <a href="Faq.html">FAQ</a>
-        <a href="Refer.html">Referral</a><br>
-        <a href="Sign Up.html">Sign Up</a>
-        <a href="Sign In.html">Sign In</a>
+        <a href="index.php">Home</a>
+        <a href="about.php">About us</a>
+        <a href="contact.php">Contact us</a>
+        <a href="Terms and Conditions.php">Terms & Conditions</a>
+        <a href="Faq.php">FAQ</a>
+        <a href="Refer.php">Referral</a><br>
+        <a href="Sign Up.php">Sign Up</a>
+        <a href="Sign In.php">Sign In</a>
     </div>
