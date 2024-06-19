da385d9f6bcf4c616d120fc9cc9024a41f99fee0
JulietMumbi <juliet.ndambuki@strathmore.edu>
2024-06-19 15:26:29 +0300

    added insertion information to my contact page

--- contact.php
+++ contact.php
@@ -9,8 +9,29 @@
 </head>
 
 <body>
-<?php include_once("templates/nav.php");?>
-<?php require_once("includes/db_connect.php");?>
+<?php 
+include_once("templates/nav.php");
+require_once("includes/db_connect.php");
+
+$First_Name = $_POST["First_Name"];
+$Last_Name = $_POST["Last_Name"];
+$gender = $_POST["gender"];
+$Mobile_Number = $_POST["Mobile_Number"];
+$email_address = $_POST["email_address"];
+
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
 
     
         <h1><img src="images/Call.png" width="5%"> Reach us:</h1>
@@ -22,15 +43,15 @@
    <p>Required fields indicated by (*)</p>
     <!--FORMS-->
    
- <form action="" >
+ <form action="<?php print htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method= "POST" class="contact_form" >
     <label for="FN">First Name*</label><br>
-    <input type="text" id="First Name" placeholder="First Name"><br><br>
+    <input type="text" name = "First_Name" id="FN" placeholder="First Name"><br><br>
 
     <label for="LN"> Last Name* </label><br>
-    <input type="text" id="First Name" placeholder="Last Name"><br><br>
+    <input type="text" name = "Last_Name "id="LN" placeholder="Last Name"><br><br>
 
     <label for="sex">Gender:</label><br>
-    <select name="" id="sex">
+    <select name="gender" id="sex">
         <option value="">---Select Gender</option>
         <option value="1">Male</option>
         <option value="2">Female</option>
@@ -38,12 +59,12 @@
     <br><br>
  
     <label for="MN">Mobile Number*</label><br>
-    <input type="text" id="Mobile Number" placeholder="Mobile Number"><br><br>
+    <input type="text" name ="Mobile_Number" id="MN" placeholder="Mobile Number"><br><br>
 
     <label for="eml">Email*</label><br>
-    <input type="text" id="Email" placeholder="Email"><br><br>
+    <input type="text" name = "email_address" id="Email" placeholder="Email"><br><br>
 
-    <input type="submit"value="Save Details"><br><br>
+    <input type="submit" name = "save details" value="Save Details"><br><br>
     <p>You can also find us on all social media platforms .</p>
     <p>We will be happy to attend to you.</p>
 
